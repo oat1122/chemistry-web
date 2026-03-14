b9f3dc961aece3a9f020a865db605d347b7ab35e
Coffee2Code <monaliza11867@gmail.com>
2026-03-14 16:34:00 +0700

    server edit upgart

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
+require("dotenv").config();
 const express = require("express");
 const next = require("next");
-/* eslint-enable @typescript-eslint/no-require-imports */
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -11,7 +10,7 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  server.all("*", (req, res) => {
+  server.all("/{*splat}", (req, res) => {
     return handle(req, res);
   });
 
